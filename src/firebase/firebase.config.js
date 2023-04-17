96f503dcfb145198e4f77b1d91b6e84926f99255
smshaown <smshaown65@gmail.com>
2023-04-18 01:49:11 +0600

    add the all file and login form now clone and practice next time

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD76LNwR9y7_heYkx-fBSg6BaHOC9cb7pI",
+  authDomain: "auth-firebase-context-ta-12d07.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-12d07",
+  storageBucket: "auth-firebase-context-ta-12d07.appspot.com",
+  messagingSenderId: "185917174476",
+  appId: "1:185917174476:web:20b24a4dc402f087120869"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
